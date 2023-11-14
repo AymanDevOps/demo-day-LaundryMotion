2a63acb734e3abe7db6e3e925b06ea914afbff8f
AymanDevOps <aymanadevops@gmail.com>
2023-11-14 14:36:40 -0500

    Update

---
+++ laundry-master copy/config/database.js
@@ -0,0 +1,5 @@
+// config/database.js
+module.exports = {
+    'url' : 'mongodb+srv://Cluster101:Projects101@cluster.nn20ndo.mongodb.net/LaundryMotion?retryWrites=true&w=majority', 
+    'dbName': 'LaundryMotion'
+};
